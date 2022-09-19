f2822bd8cdf0fec7a5fb2dfd14d7e9a0b6784369
ethan.sayagh ethan.sayagh <ethan.sayagh@gmail.com>
2022-09-19 11:16:17 +0200

    updated the contracts

--- examples/2_read_smart_contract.js
+++ examples/2_read_smart_contract.js
@@ -1,32 +1,27 @@
-const { ethers } = require("ethers");
+const { ethers } = require('ethers');
+require('dotenv').config();
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+const INFURA_URL = process.env.INFURA_URL;
+const provider = new ethers.providers.JsonRpcProvider(INFURA_URL);
 
 const ERC20_ABI = [
-    "function name() view returns (string)",
-    "function symbol() view returns (string)",
-    "function totalSupply() view returns (uint256)",
-    "function balanceOf(address) view returns (uint)",
+  'function name() view returns (string)',
+  'function symbol() view returns (string)',
+  'function totalSupply() view returns (uint256)',
+  'function balanceOf(address) view returns (uint)',
 ];
 
-const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
-const contract = new ethers.Contract(address, ERC20_ABI, provider)
+const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // DAI Contract
+const contract = new ethers.Contract(address, ERC20_ABI, provider);
 
 const main = async () => {
-    const name = await contract.name()
-    const symbol = await contract.symbol()
-    const totalSupply = await contract.totalSupply()
+  const name = await contract.name();
+  const symbol = await contract.symbol();
+  const totalSupply = await contract.totalSupply();
+  console.log('name', name);
+  console.log('symbol', symbol);
+  console.log('totalSupply', totalSupply.toString());
+  console.log('totalSupply in ether', ethers.utils.formatEther(totalSupply));
+};
 
-    console.log(`\nReading from ${address}\n`)
-    console.log(`Name: ${name}`)
-    console.log(`Symbol: ${symbol}`)
-    console.log(`Total Supply: ${totalSupply}\n`)
-
-    const balance = await contract.balanceOf('0x6c6Bc977E13Df9b0de53b251522280BB72383700')
-
-    console.log(`Balance Returned: ${balance}`)
-    console.log(`Balance Formatted: ${ethers.utils.formatEther(balance)}\n`)
-}
-
-main()
+main();
--- examples/3_send_signed_transaction.js
+++ examples/3_send_signed_transaction.js
@@ -1,34 +1,41 @@
-const { ethers } = require("ethers");
+const { ethers } = require('ethers');
+require('dotenv').config();
 
-const INFURA_ID = ''
-const provider = new ethers.providers.JsonRpcProvider(`https://kovan.infura.io/v3/${INFURA_ID}`)
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.INFURA_ROBSTEN,
+);
 
-const account1 = '' // Your account address 1
-const account2 = '' // Your account address 2
+const account1 = '0xce2839133BB0F22584747676C2d9Ab7a8a3fFdef'; // Your account address 1
+const account2 = '0xb02a2eda1b317fbd16760128836b0ac59b560e9d'; // Your account address 2
 
-const privateKey1 = '' // Private key of account 1
-const wallet = new ethers.Wallet(privateKey1, provider)
+const privateKey1 = process.env.PRIVATE_KEY; // Private key of account 1
+const wallet = new ethers.Wallet(privateKey1, provider);
 
 const main = async () => {
-    const senderBalanceBefore = await provider.getBalance(account1)
-    const recieverBalanceBefore = await provider.getBalance(account2)
+  // get balance of account 1 and 2
+  const balance1 = await provider.getBalance(account1);
+  const balance2 = await provider.getBalance(account2);
+  console.log('balance1', ethers.utils.formatEther(balance1));
+  console.log('balance2', ethers.utils.formatEther(balance2));
+  const tx = await wallet.sendTransaction({
+    to: account2,
+    value: ethers.utils.parseEther('0.025'),
+  });
 
-    console.log(`\nSender balance before: ${ethers.utils.formatEther(senderBalanceBefore)}`)
-    console.log(`reciever balance before: ${ethers.utils.formatEther(recieverBalanceBefore)}\n`)
+  await tx.wait();
+  console.log(tx);
 
-    const tx = await wallet.sendTransaction({
-        to: account2,
-        value: ethers.utils.parseEther("0.025")
-    })
+  const senderBalanceAfter = await provider.getBalance(account1);
+  const recieverBalanceAfter = await provider.getBalance(account2);
 
-    await tx.wait()
-    console.log(tx)
+  console.log(
+    `\nSender balance after: ${ethers.utils.formatEther(senderBalanceAfter)}`,
+  );
+  console.log(
+    `reciever balance after: ${ethers.utils.formatEther(
+      recieverBalanceAfter,
+    )}\n`,
+  );
+};
 
-    const senderBalanceAfter = await provider.getBalance(account1)
-    const recieverBalanceAfter = await provider.getBalance(account2)
-
-    console.log(`\nSender balance after: ${ethers.utils.formatEther(senderBalanceAfter)}`)
-    console.log(`reciever balance after: ${ethers.utils.formatEther(recieverBalanceAfter)}\n`)
-}
-
-main()
+main();
